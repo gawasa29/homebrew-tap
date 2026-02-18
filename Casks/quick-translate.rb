8d674e3d2a487351c97174f7c104608011a990b8
gawasa <gawasa@users.noreply.github.com>
2026-02-18 21:23:15 +0900

    cask: point quick-translate to v0.1.0 public release

--- Casks/quick-translate.rb
+++ Casks/quick-translate.rb
@@ -1,11 +1,11 @@
 cask "quick-translate" do
   version "0.1.0"
-  sha256 "72aa34e0e6ece8be3fa74879fcfad372e97e7d9c85ba211f61680720dc5b1734"
+  sha256 "4be3b4337c5037c3387a953a9cbfa7c277784a2a7c0758cef5c871bf3fdfc24e"
 
-  url "https://github.com/gawasa29/Projects/releases/download/macos-quick-translate-v0.1.0/QuickTranslate-macos-quick-translate-v0.1.0.zip"
+  url "https://github.com/gawasa29/macos-quick-translate/releases/download/v0.1.0/QuickTranslate-v0.1.0.zip"
   name "Quick Translate"
   desc "Quick translator in the macOS menu bar"
-  homepage "https://github.com/gawasa29/Projects/tree/main/macos-quick-translate"
+  homepage "https://github.com/gawasa29/macos-quick-translate"
 
   auto_updates false
 
