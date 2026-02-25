1f354433c31b1a195e5a4849e320747199a51e5e
gawasa29 <yusei112929@gmail.com>
2026-02-25 22:05:53 +0900

    fix: update quick-translate cask to v0.1.1

--- Casks/quick-translate.rb
+++ Casks/quick-translate.rb
@@ -1,8 +1,8 @@
 cask "quick-translate" do
-  version "0.1.0"
-  sha256 "4be3b4337c5037c3387a953a9cbfa7c277784a2a7c0758cef5c871bf3fdfc24e"
+  version "0.1.1"
+  sha256 "aea69c193c66a14ed190b3eb5e58e011e64f4ee238e5f62ed4d1a6982aa8678c"
 
-  url "https://github.com/gawasa29/macos-quick-translate/releases/download/v0.1.0/QuickTranslate-v0.1.0.zip"
+  url "https://github.com/gawasa29/macos-quick-translate/releases/download/v0.1.1/QuickTranslate-v0.1.1.zip"
   name "Quick Translate"
   desc "Quick translator in the macOS menu bar"
   homepage "https://github.com/gawasa29/macos-quick-translate"
@@ -10,6 +10,21 @@ cask "quick-translate" do
   auto_updates false
 
   app "Quick Translate.app"
+
+  postflight do
+    # Non-notarized builds may be blocked by Gatekeeper as "damaged".
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Quick Translate.app"],
+                   sudo: !appdir.writable?
+  end
+
+  caveats <<~EOS
+    If macOS still says this app is damaged, run:
+      APP_PATH="/Applications/Quick Translate.app"
+      [[ -d "$HOME/Applications/Quick Translate.app" ]] && APP_PATH="$HOME/Applications/Quick Translate.app"
+      xattr -dr com.apple.quarantine "$APP_PATH"
+      open "$APP_PATH"
+  EOS
 
   uninstall quit: "dev.gawasa.quick-translate-macos",
             delete: "~/Library/LaunchAgents/dev.gawasa.quick-translate-macos.plist"
